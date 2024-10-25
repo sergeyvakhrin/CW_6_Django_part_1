62e6aa4654551be193f9797f8267c5e49bb668b8
sergeyvakhrin <parlamentsv@mail.ru>
2024-10-25 18:27:53 +0300

    add cached

--- config/settings.py
+++ config/settings.py
@@ -153,3 +153,13 @@ SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
+CACHE_ENABLED = True
+
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379'
+        }
+    }
+
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from users.apps import UsersConfig
 from users.views import logout_view, RegisterView, ProfileView, email_verification, recovery_password, UserListView, UserDetailView, UserUpdateView, UserDeleteView
@@ -14,7 +15,7 @@ urlpatterns = [
     path("email-confirm/<str:token>/", email_verification, name='email-confirm'),
     path('recovery/', recovery_password, name='recovery'),
 
-    path('users/', UserListView.as_view(), name='user_list'),
+    path('users/', cache_page(60)(UserListView.as_view()), name='user_list'),
     path('users/<int:pk>/view', UserDetailView.as_view(), name='user_detail'),
     path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
     path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
