29cdd1b52c38fcf2f34d6945842b98b1706f995f
sergeyvakhrin <parlamentsv@mail.ru>
2024-10-29 10:12:48 +0300

    add button Blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
-from blog.views import BlogDetailView
+from blog.views import BlogDetailView, BlogListView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
+    path('blog/',cache_page(60)(BlogListView.as_view()), name='blog_list'),
     path('blog/<int:pk>/view/', BlogDetailView.as_view(), name='blog_detail'),
 ]
